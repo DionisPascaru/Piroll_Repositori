afb2877cbe8b63b6a0bdd4efc51940dc11f80fe1
step <denis.pascaru95@gmail.com>
2021-02-17 20:48:26 +0200

    17.02.2021 - Desktop finnaly version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    // testimonial slider
     $('.testimonials').slick({
         dots: true,
-        arrows: false
+        arrows: false,
+        initialSlide: 1
+    });
+
+    $('.slick-dots button').text('');
+
+    // clients slider
+    $('.clients').slick({
+        arrows: false,
+        slidesToShow: 5,
+        autoplay: true,
+        draggable: false,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
